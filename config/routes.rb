0b31a22eeceb5de8f6959ff1bd1ef6390523f7a3
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-19 12:35:27 -0600

    New users controller routes for discover and search actions

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/register', to: 'users#new', as: 'register_user'
 
+  get '/users/:id/discover', to: 'users#discover', as: 'discover_movies'
+
+  get '/users/:id/search_results', to: 'users#search_results', as: 'search_results'
+
   resources :users, only: [:show, :create] do
   end
 end
