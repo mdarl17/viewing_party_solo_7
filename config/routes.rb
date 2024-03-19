fe2b36b510021fd1450748ccb27ac23efbac2427
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-19 15:26:33 -0600

    Hand roll movies search result route, path to users/:id/movies_results; movies#index controller/action

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,11 @@ Rails.application.routes.draw do
   root "welcome#index"
   get '/register', to: 'users#new', as: 'register_user'
 
-  get '/users/:id/discover', to: 'users#discover', as: 'discover_movies'
-
-  get '/users/:id/search_results', to: 'users#search_results', as: 'search_results'
+  get '/users/:id/discover', to: 'users#search', as: 'search_movies'
+  get '/users/:id/movies', to: 'movies#index', as: 'movies_results'
 
   resources :users, only: [:show, :create] do
-
-    resources :movies, only: [:index, :show] do 
+    resources :movies, only: [:show] do 
       resources :viewing_party, only: [:new, :create]
     end
   end
