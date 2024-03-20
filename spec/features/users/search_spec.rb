b89e29d0aa06306895231af1baa0b30c9e5f32ea
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 09:37:07 -0600

    Comment out webmock; use VCR to test API calls moving forward

--- spec/features/users/search_spec.rb
+++ spec/features/users/search_spec.rb
@@ -1,26 +1,31 @@
 require "rails_helper"
 
-RSpec.describe "a user searches for movies", :vcr, type: :feature do 
-  describe "When user visits '/users/:id/discover'" do 
+RSpec.describe "a user searches for movies", :vcr, type: :feature do
+  describe "When user visits '/users/:id/movies'" do 
     before(:each) do 
-      @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
+      @user = User.create!(name: 'Tommy', email: 'tommy@email.com', id: 1)
       visit search_movies_path(@user.id)
     end
-
     it "they see a button to discover 'Top Rated Movies'" do
-      expect(page).to have_button("Find Top Rated Movies")
-
+      # json_response = File.read("spec/fixtures/webmock/top_rated.json")
+      # stub_request(:get, "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1")
+      # .with(
+      #   headers: {
+      #     'Authorization' => Rails.application.credentials.TMDB[:key]
+      #   })
+      # .to_return(status: 200, body: json_response, headers: {})
       click_button "Find Top Rated Movies"
 
       expect(current_path).to eq(movies_results_path(@user.id))
+      expect(page).to have_content("Search Results")
     end
 
-    it "they see a field to search movie titles by keyword" do 
+    it "they see a field to search movie titles by keyword", :vcr do 
       expect(page).to have_field(:keywords)
-      expect(page).to have_button("Find Movies")
-
+      fill_in(:keywords, with: "Strange Brew")
+    
       click_button "Find Movies"
-
+    
       expect(current_path).to eq(movies_results_path(@user.id))
     end
   end
