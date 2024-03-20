f512101c22efeef12908595f72fe578e6e5aca7c
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 14:42:05 -0600

    Add test for user text search that is a case-insensitive, partial matching search feature that returns all movie titles that contain a substring of the provided value entered by the user

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -9,10 +9,24 @@ RSpec.describe "Movies Results Page", :vcr, type: :feature do
     
     it "they can click the `Find Top Rated Movies` button" do 
       click_button("Find Top Rated Movies")
+
       expect(current_path).to eq(movies_results_path(@user.id))
       expect(page).to have_content("Search Results")
       expect(page.status_code).to eq 200
       expect(page).to have_content("The Shawshank Redemption")
+    end
+
+    it "a user can do a case-insensitive, partial match search for movie titles" do 
+      fill_in(:keywords, with: "Strange")
+      click_button "Find Movies"
+
+      expect(current_path).to eq(movies_results_path(@user.id))
+      
+      page.all(".movies").map.with_index do |movie_el, idx|
+        within("#results-#{idx}") do 
+          expect(page.body.downcase).to include("strange")
+        end
+      end
     end
   end
 end 
