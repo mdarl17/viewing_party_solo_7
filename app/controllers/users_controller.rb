5b627041b6f5ed229348150a5c156fa577b9dbcb
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 09:22:52 -0600

    Remove TODOs from search, moved to movies#index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,27 +4,15 @@ class UsersController < ApplicationController
    end
 
    def create
-      user = User.new(user_params)
+     user = User.new(user_params)
    end
 
-   def search
-      @user = User.find(params[:id])
-      if params[:top_movies]
-         # TODO
-         # @movies = returns TMDB's top movies hash; http call 
-         #   using `top movies` uri
-      else
-         # TODO
-         # facade method; takes advantage of ActiveRecord functions
-         #   to implememt `ILIKE` fuzzy title search
-         # @movies = facade.new.movie_search(:keywords)
-      end
-      # TODO
-      # redirect_to search results page; check project reqs
+   def discover
+     @user = User.find(params[:id])
    end
 private
 
   def user_params
-      params.permit(:name, :email)
+    params.permit(:name, :email)
   end
 end
