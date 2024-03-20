71ff72939623cd42f07868fdfd954233dfee3ebb
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 09:19:49 -0600

    Create movies index for user search results

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,18 @@
 class MoviesController < ApplicationController
   def index
+    @user = User.find(params[:id])
+    facade = MovieFacade.new
     
+    if params[:commit] == "Find Top Rated Movies"
+      @movies = facade.top_movies
+      render :index
+      return
+    elsif params[:keywords]
+      @movies = facade.keywords_search(params[:keywords])
+      @movies
+      render :index
+      return
+    end
+    render "users/search"
   end
 end
