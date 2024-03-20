7d3996a764dd9b07f0676d3e04537d413a98677e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 14:04:09 -0600

    Data helper file to represent movie objects w/out need to access external API.  Similar to 'webmock' w/out preventing external http API calls from being made

---
+++ spec/support/test_data_helper.rb
@@ -0,0 +1,45 @@
+module TestDataHelper
+  def self.movies_full_data
+    [
+      {
+          "adult": false,
+          "backdrop_path": "/8lBcqakI3F19NWkXdqE0M8W76b9.jpg",
+          "genre_ids": [
+              28,
+              18,
+              36,
+              10752
+          ],
+          "id": 197,
+          "original_language": "en",
+          "original_title": "Braveheart",
+          "overview": "Enraged at the slaughter of Murron, his new bride and childhood love, Scottish warrior William Wallace slays a platoon of the local English lord's soldiers. This leads the village to revolt and, eventually, the entire country to rise up against English rule.",
+          "popularity": 61.462,
+          "poster_path": "/or1gBugydmjToAEq7OZY0owwFk.jpg",
+          "release_date": "1995-05-24",
+          "title": "Braveheart",
+          "video": false,
+          "vote_average": 7.94,
+          "vote_count": 9670
+      },
+      {
+          "adult": false,
+          "backdrop_path": "/42l0U9K3pgIPzwtc4m0FJICijW8.jpg",
+          "genre_ids": [
+              37
+          ],
+          "id": 448394,
+          "original_language": "en",
+          "original_title": "Braveheart",
+          "overview": "Chief Standing Rock's tribe has a treaty protecting their fishing grounds, but a canning corporation is violating the treaty through intimidation and force. The tribe is divided as to how to handle the threat. Standing Rock's son, Braveheart, is sent to college to study law so that he can protect their rights, but others in the tribe, led by the hot-tempered Ki-Yote, want to provoke a more violent confrontation.",
+          "popularity": 1.949,
+          "poster_path": "/vNUIfAXXPsF8tvHnnml5lLRJyAn.jpg",
+          "release_date": "1925-12-27",
+          "title": "Braveheart",
+          "video": false,
+          "vote_average": 0.0,
+          "vote_count": 0
+      }
+    ]
+  end
+end
