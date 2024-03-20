f0a85b229c20e235d4515e18c17245725957a34e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 15:04:14 -0600

    Move before(:each) to top level to give all tests access to user and to start all tests at same path location

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -1,12 +1,12 @@
 require "rails_helper" 
 
 RSpec.describe "Movies Results Page", :vcr, type: :feature do 
+  before(:each) do 
+    @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
+    visit discover_path(@user.id)
+  end
+
   describe "two ways a user searches for movies" do 
-    before(:each) do 
-      @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
-      visit search_movies_path(@user.id)
-    end
-    
     it "they can click the `Find Top Rated Movies` button" do 
       click_button("Find Top Rated Movies")
       expect(current_path).to eq(movies_results_path(@user.id))
@@ -14,5 +14,13 @@ RSpec.describe "Movies Results Page", :vcr, type: :feature do
       expect(page.status_code).to eq 200
       expect(page).to have_content("The Shawshank Redemption")
     end
+  end
+
+  it "has a button to return to the Discover page" do 
+    click_button("Find Top Rated Movies")
+    expect(page).to have_button("Back to Discover")
+    
+    click_button "Back to Discover"
+    expect(current_path).to eq(discover_path(@user.id))
   end
 end 
