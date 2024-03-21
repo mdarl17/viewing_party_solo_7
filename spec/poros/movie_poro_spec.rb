12d4ccdb90ac794a79fe2e46638b9818f1985e5e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-21 00:54:55 -0600

    Generic movie poro now distinguished as a movie INDEX poro. Tests updated to fix breaking file name pattern.

--- spec/poros/movie_poro_spec.rb
+++ spec/poros/movie_poro_spec.rb
@@ -1,15 +1,15 @@
 require "rails_helper" 
 
-RSpec.describe MoviePoro do 
+RSpec.describe MovieIndexPoro do 
   it "exists" do 
     attrs = {
       original_title: "Shawshank Redemption",
       vote_average: 8.314
     }
 
-    movie = MoviePoro.new(attrs)
+    movie = MovieIndexPoro.new(attrs)
 
-    expect(movie).to be_a MoviePoro
+    expect(movie).to be_a MovieIndexPoro
     expect(movie.title).to eq("Shawshank Redemption")
     expect(movie.vote).to eq(8.314)
   end
