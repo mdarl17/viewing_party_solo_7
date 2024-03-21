60b812af4a79b7a7682d7e0bb3a1f11d9a6f3934
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-21 14:25:58 -0600

    Use test data (which uses a facade) to create movie data objects (movie, cast, reviews)

--- spec/features/viewing_parties/new_spec.rb
+++ spec/features/viewing_parties/new_spec.rb
@@ -1,9 +1,13 @@
 require "rails_helper" 
+require "support/test_data_helper"
 
-RSpec.describe "New Viewing Party page", type: :feature do 
+RSpec.describe "New Viewing Party page", :vcr, type: :feature do 
   before(:each) do 
     @user = User.create!(name: 'Tommy', email: 'tommy@email.com')
-    @movie_id = 264660
+    movie_details = TestDataHelper.movies_show_data(264660)
+    @movie = movie_details[:movie]
+    @cast = movie_details[:cast]
+    @reviews = movie_details[:reviews]
   end
    describe "a user creates a new viewing party" do 
     it "has a form users can fill out and submit to make a new viewing party" do 
@@ -20,5 +24,5 @@ RSpec.describe "New Viewing Party page", type: :feature do
 
       expect(current_path).to eq(viewing_party_dashboard_path)
     end
-   end
+  end
 end 
