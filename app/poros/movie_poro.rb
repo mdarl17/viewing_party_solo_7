791b544c017e5362d3dff5c37a2ef9c906e44831
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-20 18:12:59 -0600

    Start including more attributes for MoviePoro pre reqs in US3

--- app/poros/movie_poro.rb
+++ app/poros/movie_poro.rb
@@ -1,8 +1,15 @@
 class MoviePoro 
-  attr_reader :title, :vote
+  attr_reader :id, :title, :vote
 
   def initialize(movie_data)
+    @id = movie_data[:id]
     @title = movie_data[:original_title]
     @vote = movie_data[:vote_average]
+    @runtime = movie_data[:runtime]
+    @genres = movie_data[:genres]
+    @summary = movie_data[:overview]
+    # TODO Cast will be diff url
+    # TODO Reviews will be diff url
+        # TODO Part of Reviews => :author, :author_details
   end
 end
