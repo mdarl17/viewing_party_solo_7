864712b83225ef0e0a179658a3f53d81d9f0da30
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-22 10:05:49 -0600

    Start of feature tests for user dashboard

--- spec/features/users/show_spec.rb
+++ spec/features/users/show_spec.rb
@@ -1,7 +1,11 @@
 require "rails_helper"
 
-RSpec.describe "User Dashboard", type: :type do 
-  describe "" do 
+RSpec.describe "User Dashboard", type: :type do
+  before(:each) do 
+    load_test_data
+  end
+
+  it "displays the viewing parties the user is attending or hosting" do 
     
   end
 end 
