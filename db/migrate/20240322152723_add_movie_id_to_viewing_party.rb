17cf9be4674f5c11ed8d9ac54a874eba0fb06115
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-22 13:50:56 -0600

    Migration to change viewing_party.movie_id from string to integer

--- db/migrate/20240322152723_add_movie_id_to_viewing_party.rb
+++ db/migrate/20240322152723_add_movie_id_to_viewing_party.rb
@@ -1,5 +1,5 @@
 class AddMovieIdToViewingParty < ActiveRecord::Migration[7.1]
   def change
-    add_column :viewing_parties, :movie_id, :string
+    add_column :viewing_parties, :movie_id, :integer
   end
 end
