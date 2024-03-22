77c461006534fe06542578d2c12cddea94c96ca5
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-21 23:45:53 -0600

    New #show action for new user dashboard pagae

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,20 @@
 class UsersController < ApplicationController
 
-   def new
-   end
+  def new
+  end
 
-   def create
-     user = User.new(user_params)
-   end
+  def create
+    user = User.new(user_params)
+  end
 
-   def discover
-     @user = User.find(params[:id])
-   end
+  def discover
+    @user = User.find(params[:id])
+  end
+
+  def show 
+    @user = User.find(params[:user_id])
+  end
+
 private
 
   def user_params
