d52ee37c9cc4b5781d39caf9d2e98520cf03a84e
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-21 23:46:19 -0600

    New feature test for viewing parties - with and w/out happy & sad path testing

--- app/controllers/viewing_parties_controller.rb
+++ app/controllers/viewing_parties_controller.rb
@@ -1,5 +1,47 @@
 class ViewingPartiesController < ApplicationController 
   def new 
-    
+    @users = User.all
+    @user = User.find(params[:user_id])
+    facade = MovieFacade.new
+    movie_data = movies_show_data(params[:movie_id])
+    @movie = movie_data[:movie]
+  end
+
+  def create
+    movie_data = movies_show_data(params[:movie_id])
+    @movie = movie_data[:movie]
+    @user = User.find(params[:user_id])
+    unless ViewingParty.duration_valid?(params, @movie)
+      flash[:alert] = "The party duration can't be shorter than the movie runtime"
+      redirect_to new_viewing_party_path(user_id: @user.id, movie_id: @movie.id)
+      return
+    end
+
+    viewing_party = ViewingParty.new(viewing_party_params)
+
+    if viewing_party.save
+      viewing_party.user_parties.create(viewing_party_id: viewing_party.id, user_id: params[:user_id], host: true)
+      guests = viewing_party.parse_guests(params)
+
+      guests.each do |guest| 
+        viewing_party.user_parties.create(user_party_params)
+      end
+      flash[:message] = "New viewing party created!"
+      redirect_to user_dashboard_path(@user.id)
+      return
+    else
+      flash[:alert] = "Sorry, but there was a problem adding the party. Please try again later."
+      redirect_to new_viewing_party_path(user_id: @user.id, movie_id: @movie.id)
+    end
+  end
+
+  private
+
+  def viewing_party_params 
+    params.permit(:viewing_party_id, :duration, :date, :start_time)
+  end
+
+  def user_party_params 
+    params.permit(:viewing_party_id, :user_id, :host)
   end
 end
