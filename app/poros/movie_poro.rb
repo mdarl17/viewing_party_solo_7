09110030a8008fd3f79ef61965716ee7fc63d7d9
Matt Darlington <matthewsdarlington@gmail.com>
2024-03-23 18:11:25 -0600

    Helper methods needed to retrieve actual genre strings instead of codes. Two endpoints with differing data structures, specifically with genres, need helpers to ensure correct genre strings are stored in @genres instance variable array regardless of API endpoint.

--- app/poros/movie_poro.rb
+++ app/poros/movie_poro.rb
@@ -3,16 +3,28 @@ class MoviePoro
 
   def initialize(attributes)
     @id = attributes[:id]
-    @title = attributes[:original_title]
+    @title = attributes[:title]
     @vote = attributes[:vote_average]
     @runtime = attributes[:runtime]
-    @genres = parse_genres(attributes[:genres])
+    # if search with movie_id => 
+    @genres = attributes[:genres] ? parse_genres(attributes[:genres]) : decode_genre_ids(attributes[:genre_ids])
     @summary = attributes[:overview]
   end
 
-  def parse_genres(genre_data)
-    genre_data.map do |genre|
-      genre[:name]
+  def decode_genre_ids(genre_ids) 
+    return [] unless genre_ids
+    genre_code_hashes = GenreDecoder.new.genre_hash[:genres]
+    genre_code_hashes.reduce([]) do |genre_list, code_hash| 
+      if genre_ids.include?(code_hash[:id])
+        genre_list << code_hash[:name]
+      end
+      genre_list
+    end
+  end
+
+  def parse_genres(genre_hash_array)
+    genre_names = genre_hash_array.map do |genre_hash| 
+      genre_hash[:name]
     end
   end
 end
